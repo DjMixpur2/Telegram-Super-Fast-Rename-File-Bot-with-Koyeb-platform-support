b502d47df185260c5bd5802def51bdc28b93bace
DjMixpur2 <djmixpur2@gmail.com>
2025-01-12 19:41:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os
 
 class Config:
-    BOT_TOKEN = os.getenv("BOT_TOKEN")  # Telegram bot token
-    API_ID = int(os.getenv("API_ID"))  # Telegram API ID
-    API_HASH = os.getenv("API_HASH")  # Telegram API hash
+    BOT_TOKEN = os.getenv("7994263128:AAE7dWuOWMjZSgr6C2XIODltb0ZOQ5laVQQ")  # Telegram bot token
+    API_ID = int(os.getenv("12850056"))  # Telegram API ID
+    API_HASH = os.getenv("15564ec4a1a2cbef87c99a9aa9e40b34")  # Telegram API hash
     KOYEB_WEBHOOK = os.getenv("KOYEB_WEBHOOK")  # Koyeb Webhook URL
-    WATERMARK_TEXT = "Your Watermark"  # Customize this text for watermark
+    WATERMARK_TEXT = "@KrBackup"  # Customize this text for watermark
     THUMBNAIL_PATH = os.getenv("THUMBNAIL_PATH", "thumbnail.jpg")  # Custom thumbnail path
